9c8c24d405bd127d521c979ff78c4c25fc3797f6
文壮 <wenzhuang@meituan.com>
2019-10-19 18:03:02 +0800

    backtracking: n queues

---
+++ src/test/the_beauty_of_data_structure_and_algorithm/algorithm/backtracking/example/NQueueTest.java
@@ -0,0 +1,13 @@
+package test.the_beauty_of_data_structure_and_algorithm.algorithm.backtracking.example;
+
+import main.the_beauty_of_data_structure_and_algorithm.algorithm.backtracking.example.NQueue;
+
+/**
+ * @author wenzhuang
+ * @date 2019-10-19 17:36
+ */
+public class NQueueTest {
+    public static void main(String[] args){
+        new NQueue().nQueue(8);
+    }
+}
